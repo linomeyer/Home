50b356b1f6d6073218879fbbfa98c02521bba311
Lino Meyer <linomeyer02@gmail.com>
2018-09-13 08:54:51 +0200

    color identifier for check

--- ZebChe/src/tech/bison/trainee17/zebche/Chessboard.java
+++ ZebChe/src/tech/bison/trainee17/zebche/Chessboard.java
@@ -18,220 +18,219 @@ import tech.bison.trainee17.zebche.pieces.Queen;
 import tech.bison.trainee17.zebche.pieces.Rook;
 
 public class Chessboard {
-	
-  public enum StateOfKing {
-	  OK, CHECK, CHECKMATE	  
-  }
-	
-  public final int height;
-  public final int length;
-  private HashMap<Square, Piece> chessboard;
 
-  /**
-   * Standard Chessboard 8x8 with 16 black and 16 white pieces
-   **/
-  public Chessboard() {
-    height = 8;
-    length = 8;
-    chessboard = initializeChessboard();
-  }
+	public enum StateOfKing {
+		OK, CHECK, CHECKMATE
+	}
 
-  public Chessboard(int height, int length) {
-    this.height = height;
-    this.length = length;
-    chessboard = new HashMap<>();
-  }
+	public final int height;
+	public final int length;
+	private HashMap<Square, Piece> chessboard;
 
-  public HashMap<Square, Piece> initializeChessboard() {
-    HashMap<Square, Piece> board = new HashMap<Square, Piece>();
+	/**
+	 * Standard Chessboard 8x8 with 16 black and 16 white pieces
+	 **/
+	public Chessboard() {
+		height = 8;
+		length = 8;
+		chessboard = initializeChessboard();
+	}
 
-    board.put(new Square("H8"), new Rook(Color.BLACK));
-    board.put(new Square("A8"), new Rook(Color.BLACK));
+	public Chessboard(int height, int length) {
+		this.height = height;
+		this.length = length;
+		chessboard = new HashMap<>();
+	}
 
-    board.put(new Square("B8"), new Knight(Color.BLACK));
-    board.put(new Square("G8"), new Knight(Color.BLACK));
+	public HashMap<Square, Piece> initializeChessboard() {
+		HashMap<Square, Piece> board = new HashMap<Square, Piece>();
 
-    board.put(new Square("C8"), new Bishop(Color.BLACK));
-    board.put(new Square("F8"), new Bishop(Color.BLACK));
+		board.put(new Square("H8"), new Rook(Color.BLACK));
+		board.put(new Square("A8"), new Rook(Color.BLACK));
 
-    board.put(new Square("D8"), new Queen(Color.BLACK));
+		board.put(new Square("B8"), new Knight(Color.BLACK));
+		board.put(new Square("G8"), new Knight(Color.BLACK));
 
-    board.put(new Square("E8"), new King(Color.BLACK));
+		board.put(new Square("C8"), new Bishop(Color.BLACK));
+		board.put(new Square("F8"), new Bishop(Color.BLACK));
 
-    board.put(new Square("A7"), new Pawn(Color.BLACK));
-    board.put(new Square("B7"), new Pawn(Color.BLACK));
-    board.put(new Square("C7"), new Pawn(Color.BLACK));
-    board.put(new Square("D7"), new Pawn(Color.BLACK));
-    board.put(new Square("E7"), new Pawn(Color.BLACK));
-    board.put(new Square("F7"), new Pawn(Color.BLACK));
-    board.put(new Square("G7"), new Pawn(Color.BLACK));
-    board.put(new Square("H7"), new Pawn(Color.BLACK));
+		board.put(new Square("D8"), new Queen(Color.BLACK));
 
-    board.put(new Square("A1"), new Rook(Color.WHITE));
-    board.put(new Square("H1"), new Rook(Color.WHITE));
+		board.put(new Square("E8"), new King(Color.BLACK));
 
-    board.put(new Square("B1"), new Knight(Color.WHITE));
-    board.put(new Square("G1"), new Knight(Color.WHITE));
+		board.put(new Square("A7"), new Pawn(Color.BLACK));
+		board.put(new Square("B7"), new Pawn(Color.BLACK));
+		board.put(new Square("C7"), new Pawn(Color.BLACK));
+		board.put(new Square("D7"), new Pawn(Color.BLACK));
+		board.put(new Square("E7"), new Pawn(Color.BLACK));
+		board.put(new Square("F7"), new Pawn(Color.BLACK));
+		board.put(new Square("G7"), new Pawn(Color.BLACK));
+		board.put(new Square("H7"), new Pawn(Color.BLACK));
 
-    board.put(new Square("C1"), new Bishop(Color.WHITE));
-    board.put(new Square("F1"), new Bishop(Color.WHITE));
+		board.put(new Square("A1"), new Rook(Color.WHITE));
+		board.put(new Square("H1"), new Rook(Color.WHITE));
 
-    board.put(new Square("D1"), new Queen(Color.WHITE));
+		board.put(new Square("B1"), new Knight(Color.WHITE));
+		board.put(new Square("G1"), new Knight(Color.WHITE));
 
-    board.put(new Square("E1"), new King(Color.WHITE));
+		board.put(new Square("C1"), new Bishop(Color.WHITE));
+		board.put(new Square("F1"), new Bishop(Color.WHITE));
 
-    board.put(new Square("A2"), new Pawn(Color.WHITE));
-    board.put(new Square("B2"), new Pawn(Color.WHITE));
-    board.put(new Square("C2"), new Pawn(Color.WHITE));
-    board.put(new Square("D2"), new Pawn(Color.WHITE));
-    board.put(new Square("E2"), new Pawn(Color.WHITE));
-    board.put(new Square("F2"), new Pawn(Color.WHITE));
-    board.put(new Square("G2"), new Pawn(Color.WHITE));
-    board.put(new Square("H2"), new Pawn(Color.WHITE));
-    return board;
-  }
+		board.put(new Square("D1"), new Queen(Color.WHITE));
 
-  public boolean isAValidSquare(Square square) {
-    if (square.getXCoordinate() >= 0 && square.getXCoordinate() <= height && square.getYCoordinate() >= 0
-        && square.getYCoordinate() <= length) {
-      return true;
-    } else {
-      return false;
-    }
-  }
+		board.put(new Square("E1"), new King(Color.WHITE));
 
-  public boolean isAValidMoveOfPiece(Movement movement) throws InvalidSquareException, EmptySquareException {
-    boolean validMoveOfPiece = getPiece(movement.getStart()).isAValidMove(movement);
-    return validMoveOfPiece;
-  }
+		board.put(new Square("A2"), new Pawn(Color.WHITE));
+		board.put(new Square("B2"), new Pawn(Color.WHITE));
+		board.put(new Square("C2"), new Pawn(Color.WHITE));
+		board.put(new Square("D2"), new Pawn(Color.WHITE));
+		board.put(new Square("E2"), new Pawn(Color.WHITE));
+		board.put(new Square("F2"), new Pawn(Color.WHITE));
+		board.put(new Square("G2"), new Pawn(Color.WHITE));
+		board.put(new Square("H2"), new Pawn(Color.WHITE));
+		return board;
+	}
 
-  public HashMap<Square, Piece> getChessboard() {
-    return chessboard;
-  }
+	public boolean isAValidSquare(Square square) {
+		if (square.getXCoordinate() >= 0 && square.getXCoordinate() <= height && square.getYCoordinate() >= 0
+				&& square.getYCoordinate() <= length) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-  public Piece getPiece(Square square) throws InvalidSquareException, EmptySquareException {
-    try {
-      if (isAValidSquare(square)) {
-        if (chessboard.get(square) == null) {
-          throw new EmptySquareException();
-        }
-        return chessboard.get(square);
-      } else {
-        throw new InvalidSquareException();
-      }
-    } catch (NullPointerException e) {
-      throw new EmptySquareException();
-    }
-  }
+	public boolean isAValidMoveOfPiece(Movement movement) throws InvalidSquareException, EmptySquareException {
+		boolean validMoveOfPiece = getPiece(movement.getStart()).isAValidMove(movement);
+		return validMoveOfPiece;
+	}
 
-  public Piece removePiece(Square square) throws InvalidSquareException, EmptySquareException {
-    if (isAValidSquare(square)) {
-      if (chessboard.get(square) == null) {
-        throw new EmptySquareException();
-      }
-      return chessboard.remove(square);
-    } else {
-      throw new InvalidSquareException();
-    }
-  }
+	public HashMap<Square, Piece> getChessboard() {
+		return chessboard;
+	}
 
-  public void addPiece(Square square, Piece piece) throws SquareOccupiedException, InvalidSquareException {
-    if (chessboard.get(square) == null) {
-      if (isAValidSquare(square)) {
-        chessboard.put(square, piece);
-      } else {
-        throw new InvalidSquareException();
-      }
-    } else {
-      throw new SquareOccupiedException();
-    }
-  }
+	public Piece getPiece(Square square) throws InvalidSquareException, EmptySquareException {
+		try {
+			if (isAValidSquare(square)) {
+				if (chessboard.get(square) == null) {
+					throw new EmptySquareException();
+				}
+				return chessboard.get(square);
+			} else {
+				throw new InvalidSquareException();
+			}
+		} catch (NullPointerException e) {
+			throw new EmptySquareException();
+		}
+	}
 
-  public boolean isWayEmpty(Movement movement) {
-    ArrayList<Square> squaresOnWay = movement.getWay();
+	public Piece removePiece(Square square) throws InvalidSquareException, EmptySquareException {
+		if (isAValidSquare(square)) {
+			if (chessboard.get(square) == null) {
+				throw new EmptySquareException();
+			}
+			return chessboard.remove(square);
+		} else {
+			throw new InvalidSquareException();
+		}
+	}
 
-    for (Square square : squaresOnWay) {
-      try {
-        if (getPiece(square) != null) {
-          return false;
-        }
-      } catch (Exception e) {
-      }
-    }
-    return true;
-  }
-  
-  public boolean isKingInCheck() {
-	ArrayList<Square> squaresOfKing = getSquaresOfKings();
-	Set<Square> squaresOfAllPieces = chessboard.keySet();
-	
-	for (Square squareOfKing : squaresOfKing) {
+	public void addPiece(Square square, Piece piece) throws SquareOccupiedException, InvalidSquareException {
+		if (chessboard.get(square) == null) {
+			if (isAValidSquare(square)) {
+				chessboard.put(square, piece);
+			} else {
+				throw new InvalidSquareException();
+			}
+		} else {
+			throw new SquareOccupiedException();
+		}
+	}
+
+	public boolean isWayEmpty(Movement movement) {
+		ArrayList<Square> squaresOnWay = movement.getWay();
+
+		for (Square square : squaresOnWay) {
+			try {
+				if (getPiece(square) != null) {
+					return false;
+				}
+			} catch (Exception e) {
+			}
+		}
+		return true;
+	}
+
+	public boolean isKingInCheck(Color color) {
+		Square squareOfKing = getSquareOfKing(color);
+		Set<Square> squaresOfAllPieces = chessboard.keySet();
+
 		for (Square squareOfPiece : squaresOfAllPieces) {
 			Movement movement = new Movement(squareOfPiece, squareOfKing);
 			try {
-				if (isAValidMoveOfPiece(movement) && isWayEmpty(movement) && getPiece(squareOfPiece).color != getPiece(squareOfKing).color) {
+				if (isAValidMoveOfPiece(movement) && isWayEmpty(movement)
+						&& getPiece(squareOfPiece).color != getPiece(squareOfKing).color) {
 					return true;
 				} else {
 				}
 			} catch (Exception e) {
 			}
 		}
+		return false;
 	}
-	return false;
-  }
-  
-  public ArrayList<Square> getSquaresOfKings() {
-	  Set<Square> squares = chessboard.keySet();
-	  ArrayList<Square> squaresOfKings = new ArrayList<>();
-	  
-	  for (Square square : squares) {
-		try {
-			if (getPiece(square).equals(new King(Color.WHITE)) || getPiece(square).equals(new King(Color.BLACK))) {
-				squaresOfKings.add(square);
-			}
-		} catch (Exception e) {
-		}
-	}
-	return squaresOfKings;
-}
 
-@Override
-  public boolean equals(Object obj) {
-    if (this == obj)
-      return true;
-    if (obj == null)
-      return false;
-    if (getClass() != obj.getClass())
-      return false;
-    Chessboard other = (Chessboard) obj;
-    if (chessboard == null) {
-      if (other.chessboard != null)
-        return false;
-    } else if (!chessboard.equals(other.chessboard))
-      return false;
-    if (height != other.height)
-      return false;
-    if (length != other.length)
-      return false;
-    return true;
-  }
-  
-  @Override
-	public String toString(){
-	  String chessboardAsString = "";
-		  for (int i = height; i > 0 ; i--) {
-			  chessboardAsString += i + " |";
-			  for(int j = 1; j <= length; j++) {
-				  try {
+	public Square getSquareOfKing(Color color) {
+		Set<Square> squares = chessboard.keySet();
+		Square squareOfKing = null;
+
+		for (Square square : squares) {
+			try {
+				if (getPiece(square).equals(new King(color))) {
+					squareOfKing = square;
+				}
+			} catch (Exception e) {
+			}
+		}
+		return squareOfKing;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Chessboard other = (Chessboard) obj;
+		if (chessboard == null) {
+			if (other.chessboard != null)
+				return false;
+		} else if (!chessboard.equals(other.chessboard))
+			return false;
+		if (height != other.height)
+			return false;
+		if (length != other.length)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		String chessboardAsString = "";
+		for (int i = height; i > 0; i--) {
+			chessboardAsString += i + " |";
+			for (int j = 1; j <= length; j++) {
+				try {
 					chessboardAsString += this.getPiece(new Square(j, i)).toString();
 				} catch (Exception e) {
 					chessboardAsString += ChessboardPrinter.SPACE;
 				}
-				  chessboardAsString += "|";
-			  }
-			  chessboardAsString += "\n";
-		  }
-		  chessboardAsString += "   A  B  C  D  E  F  G  H";
-	return chessboardAsString;
+				chessboardAsString += "|";
+			}
+			chessboardAsString += "\n";
+		}
+		chessboardAsString += "   A  B  C  D  E  F  G  H";
+		return chessboardAsString;
 	}
 }
--- ZebChe/src_test/tech/bison/trainee17/zebche/ChessboardTest.java
+++ ZebChe/src_test/tech/bison/trainee17/zebche/ChessboardTest.java
@@ -1,14 +1,11 @@
 package tech.bison.trainee17.zebche;
 
-import static org.junit.Assert.*;
-
-import java.util.ArrayList;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
-
-import static org.hamcrest.CoreMatchers.is;
 
 import tech.bison.trainee17.zebche.exceptions.EmptySquareException;
 import tech.bison.trainee17.zebche.pieces.Bishop;
@@ -20,117 +17,118 @@ import tech.bison.trainee17.zebche.pieces.Rook;
 
 public class ChessboardTest {
 
-  @Rule
-  public ExpectedException thrown = ExpectedException.none();
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
 
-  @Test
-  public void chessboard_getPieceOfInitializedChessboard_Rook() throws Exception {
-    Chessboard chessboard = new Chessboard();
+	@Test
+	public void chessboard_getPieceOfInitializedChessboard_Rook() throws Exception {
+		Chessboard chessboard = new Chessboard();
 
-    Piece piece = chessboard.getPiece(new Square("A1"));
+		Piece piece = chessboard.getPiece(new Square("A1"));
 
-    assertThat(piece, is(new Rook(Color.WHITE)));
-  }
+		assertThat(piece, is(new Rook(Color.WHITE)));
+	}
 
-  @Test
-  public void chessboard_square_isNotAValidSquare() throws Exception {
-    Chessboard chessboard = new Chessboard();
+	@Test
+	public void chessboard_square_isNotAValidSquare() throws Exception {
+		Chessboard chessboard = new Chessboard();
 
-    boolean validSquare = chessboard.isAValidSquare(new Square("Z1"));
+		boolean validSquare = chessboard.isAValidSquare(new Square("Z1"));
 
-    assertThat(validSquare, is(false));
-  }
+		assertThat(validSquare, is(false));
+	}
 
-  @Test
-  public void chessboard_addPiece_pieceAdded() throws Exception {
-    Chessboard chessboard = new Chessboard();
+	@Test
+	public void chessboard_addPiece_pieceAdded() throws Exception {
+		Chessboard chessboard = new Chessboard();
 
-    chessboard.addPiece(new Square("A4"), new Knight(Color.BLACK));
+		chessboard.addPiece(new Square("A4"), new Knight(Color.BLACK));
 
-    assertThat(chessboard.getPiece(new Square("A4")), is(new Knight(Color.BLACK)));
-  }
+		assertThat(chessboard.getPiece(new Square("A4")), is(new Knight(Color.BLACK)));
+	}
 
-  @Test
-  public void chessboard_removePiece_pieceRemoved() throws Exception {
-    Chessboard chessboard = new Chessboard();
+	@Test
+	public void chessboard_removePiece_pieceRemoved() throws Exception {
+		Chessboard chessboard = new Chessboard();
 
-    Piece removedPiece = chessboard.removePiece(new Square("H8"));
+		Piece removedPiece = chessboard.removePiece(new Square("H8"));
 
-    assertThat(removedPiece, is(new Rook(Color.BLACK)));
+		assertThat(removedPiece, is(new Rook(Color.BLACK)));
 
-    thrown.expect(EmptySquareException.class);
-    chessboard.getPiece(new Square("H8"));
-  }
+		thrown.expect(EmptySquareException.class);
+		chessboard.getPiece(new Square("H8"));
+	}
 
-  @Test
-  public void chessboard_removePiece_emptySquare() throws Exception {
-    Chessboard chessboard = new Chessboard();
+	@Test
+	public void chessboard_removePiece_emptySquare() throws Exception {
+		Chessboard chessboard = new Chessboard();
 
-    thrown.expect(EmptySquareException.class);
-    chessboard.removePiece(new Square("D5"));
-  }
+		thrown.expect(EmptySquareException.class);
+		chessboard.removePiece(new Square("D5"));
+	}
 
-  @Test
-  public void chessboard_move_wayIsNotEmpty() throws Exception {
-    Chessboard chessboard = new Chessboard();
+	@Test
+	public void chessboard_move_wayIsNotEmpty() throws Exception {
+		Chessboard chessboard = new Chessboard();
 
-    boolean isWayEmpty = chessboard.isWayEmpty(new Movement(new Square("A1"), new Square("A4")));
+		boolean isWayEmpty = chessboard.isWayEmpty(new Movement(new Square("A1"), new Square("A4")));
 
-    assertThat(isWayEmpty, is(false));
-  }
+		assertThat(isWayEmpty, is(false));
+	}
 
-  @Test
-  public void chessboard_move_wayIsEmpty() throws Exception {
-    Chessboard chessboard = new Chessboard();
+	@Test
+	public void chessboard_move_wayIsEmpty() throws Exception {
+		Chessboard chessboard = new Chessboard();
 
-    boolean wayEmpty = chessboard.isWayEmpty(new Movement(new Square("D5"), new Square("G5")));
+		boolean wayEmpty = chessboard.isWayEmpty(new Movement(new Square("D5"), new Square("G5")));
 
-    assertThat(wayEmpty, is(true));
-  }
-  
-  @Test
-  public void chessboard_isKingInCheck_true() throws Exception {
-	Chessboard chessboard = new Chessboard(8, 8);
-	
-	chessboard.addPiece(new Square("C3"), new King(Color.WHITE));
-	chessboard.addPiece(new Square("C6"), new Rook(Color.BLACK));
-	
-	boolean kingInCheck = chessboard.isKingInCheck();
-	
-	assertThat(kingInCheck, is(true));
-}
-  
-  @Test
-public void chessboard_isKingInCheck_false() throws Exception {
-	Chessboard chessboard = new Chessboard(8,8);
-	
-	chessboard.addPiece(new Square("D2"), new King(Color.BLACK));
-	chessboard.addPiece(new Square("A8"), new King(Color.WHITE));
-	chessboard.addPiece(new Square("D3"), new Bishop(Color.WHITE));
-	
-	boolean kingInCheck = chessboard.isKingInCheck();
-	
-	assertThat(kingInCheck, is(false));
-}
-  @Test
-public void chessboard_isKingInCheck_falseBecauseOfDifferentColor() throws Exception {
-	Chessboard chessboard = new Chessboard(8, 8);
-	
-	chessboard.addPiece(new Square("C3"), new King(Color.BLACK));
-	chessboard.addPiece(new Square("C6"), new Rook(Color.BLACK));
-	
-	boolean kingInCheck = chessboard.isKingInCheck();
-	
-	assertThat(kingInCheck, is(false));
-}
-  
-  @Test
-  public void chessboard_getSquaresOfKings_squaresOfKings() throws Exception {
-	Chessboard chessboard = new Chessboard();
-	
-	ArrayList<Square> squaresOfKings = chessboard.getSquaresOfKings();
-	
-	assertThat(squaresOfKings.toArray(), is(new Square[] {new Square("E1"), new Square("E8")}));
-  }
-  
+		assertThat(wayEmpty, is(true));
+	}
+
+	@Test
+	public void chessboard_isKingInCheck_true() throws Exception {
+		Chessboard chessboard = new Chessboard(8, 8);
+
+		chessboard.addPiece(new Square("C3"), new King(Color.WHITE));
+		chessboard.addPiece(new Square("C6"), new Rook(Color.BLACK));
+
+		boolean kingInCheck = chessboard.isKingInCheck(Color.WHITE);
+
+		assertThat(kingInCheck, is(true));
+	}
+
+	@Test
+	public void chessboard_isKingInCheck_false() throws Exception {
+		Chessboard chessboard = new Chessboard(8, 8);
+
+		chessboard.addPiece(new Square("D2"), new King(Color.BLACK));
+		chessboard.addPiece(new Square("A8"), new King(Color.WHITE));
+		chessboard.addPiece(new Square("D3"), new Bishop(Color.WHITE));
+
+		boolean kingInCheck = chessboard.isKingInCheck(Color.BLACK);
+
+		assertThat(kingInCheck, is(false));
+	}
+
+	@Test
+	public void chessboard_isKingInCheck_falseBecauseOfDifferentColor() throws Exception {
+		Chessboard chessboard = new Chessboard(8, 8);
+
+		chessboard.addPiece(new Square("C3"), new King(Color.BLACK));
+		chessboard.addPiece(new Square("C6"), new Rook(Color.BLACK));
+
+		boolean kingInCheck = chessboard.isKingInCheck(Color.BLACK);
+
+		assertThat(kingInCheck, is(false));
+	}
+
+	@Test
+	public void chessboard_getSquaresOfKings_squaresOfKings() throws Exception {
+		Chessboard chessboard = new Chessboard();
+
+		Square squaresOfKings = chessboard.getSquareOfKing(Color.WHITE);
+
+		assertThat(squaresOfKings, is(new Square("E1")));
+	}
+
 }
