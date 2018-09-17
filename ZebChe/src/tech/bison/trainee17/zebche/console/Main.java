87bdcbaace6cd9bd811d2111e0c4367afb7e1757
Lino Meyer <linomeyer02@gmail.com>
2018-09-17 15:27:10 +0200

    .

--- ZebChe/src/tech/bison/trainee17/zebche/console/Main.java
+++ ZebChe/src/tech/bison/trainee17/zebche/console/Main.java
@@ -1,14 +1,6 @@
 package tech.bison.trainee17.zebche.console;
 
-import tech.bison.trainee17.zebche.Chessboard;
-
 public class Main {
 	public static void main(String[] args) {
-		boolean isKingInCheckMate = false;
-		Chessboard chessboard = new Chessboard();
-
-		while (isKingInCheckMate) {
-
-		}
 	}
 }
